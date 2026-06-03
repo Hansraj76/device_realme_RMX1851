00913f1554cbc25607ee8357a1c1687623278e2e
Hansraj76 <gamerx8x6x@gmail.com>
2026-06-03 17:10:32 +0530

    RMX1851:Bringup for Infinity-X & Drop shipping rui2fw with rom

--- infinity_RMX1851.mk
+++ infinity_RMX1851.mk
@@ -7,7 +7,10 @@
 # Inherit some common LineageOS stuff
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_ENABLE_BLUR :=false
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+INFINITY_MAINTAINER := Sourabh
+WITH_GAPPS := false
+PERF_ANIM_OVERRIDE := true
+$(call inherit-product, vendor/infinity/config/common_full_phone.mk)
 
 # Inherit from RMX1851 device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -15,7 +18,7 @@ $(call inherit-product, $(LOCAL_PATH)/device.mk)
 PRODUCT_BRAND := realme
 PRODUCT_DEVICE := RMX1851
 PRODUCT_MANUFACTURER := realme
-PRODUCT_NAME := lineage_RMX1851
+PRODUCT_NAME := infinity_RMX1851
 PRODUCT_MODEL := RMX1851
 
 PRODUCT_GMS_CLIENTID_BASE := android-oppo
